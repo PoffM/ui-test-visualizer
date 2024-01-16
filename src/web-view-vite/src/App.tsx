48c21c2e8655391e33d2adab7ac1a94f5303b49c
Mathew Poff <matp@live.ca>
2024-01-15 20:49:45 -0500

    Set up @vscode/webview-ui-toolkit in the Vite app.

--- src/web-view-vite/src/App.tsx
+++ src/web-view-vite/src/App.tsx
@@ -1,3 +1,19 @@
+import * as webviewToolkit from "@vscode/webview-ui-toolkit";
+
+// In order to use the Webview UI Toolkit web components they
+// must be registered with the browser (i.e. webview) using the
+// syntax below.
+webviewToolkit
+  .provideVSCodeDesignSystem()
+  .register(webviewToolkit.vsCodeButton());
+
 export function App() {
-  return <div class="text-green-400">Welcome to the web view</div>;
+  return (
+    <div class="fixed inset-0 flex justify-center items-center">
+      <div class="flex flex-col gap-4">
+        <div class="h-[500px] w-[500px] bg-gray-500 bg-opacity-20">hello</div>
+        <vscode-button appearance="primary">Button Text</vscode-button>
+      </div>
+    </div>
+  );
 }
