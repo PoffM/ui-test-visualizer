51af4a5e19c08bfd74fb24047d8993e7b17ee6ea
Mathew Poff <matp@live.ca>
2024-01-17 00:44:43 -0500

    Made the html viewer resizable

--- src/web-view-vite/src/App.tsx
+++ src/web-view-vite/src/App.tsx
@@ -1,6 +1,7 @@
 import { createEventSignal } from "@solid-primitives/event-listener";
 import * as webviewToolkit from "@vscode/webview-ui-toolkit";
-import { Show, createEffect, createMemo } from "solid-js";
+import { Show, createEffect, createSignal } from "solid-js";
+import { Resizable } from "./lib/Resizable";
 
 // In order to use the Webview UI Toolkit web components they
 // must be registered with the browser (i.e. webview) using the
@@ -10,19 +11,26 @@ webviewToolkit
   .register(webviewToolkit.vsCodeButton());
 
 export function App() {
-  // Receive the test DOM's HTML from the extension's back-end.
   const lastMessage = createEventSignal(window, "message");
-  const testHtml = createMemo(() => String(lastMessage()?.data?.newHtml));
 
-  function shadowHostRef(el: HTMLElement) {
+  // Receive the test DOM's HTML from the extension's back-end.
+  const [testHtml, setTestHtml] = createSignal<string>("");
+  createEffect(() => {
+    if (lastMessage()?.data?.newHtml) {
+      setTestHtml(String(lastMessage().data.newHtml));
+    }
+  });
+
+  /** Display the test's HTML with isolated CSS. */
+  function initShadowDom(el: HTMLDivElement) {
     const shadow = el.attachShadow({ mode: "open" });
     createEffect(() => (shadow.innerHTML = testHtml()));
   }
 
   return (
-    <div class="fixed inset-0 flex justify-center items-center">
-      <div class="flex flex-col gap-4 w-[500px] max-w-[100vw]">
-        <div class="h-[500px] bg-gray-500 bg-opacity-20">
+    <div class="fixed inset-0 flex flex-col gap-4 justify-center items-center">
+      <Resizable initialWidth={500}>
+        <div class="h-[500px] bg-gray-500 bg-opacity-20 p-2">
           <Show
             when={testHtml()}
             fallback={
@@ -32,12 +40,13 @@ export function App() {
             }
           >
             <div class="h-full w-full">
-              <div ref={shadowHostRef} />
+              <div ref={initShadowDom} />
             </div>
           </Show>
         </div>
-        <vscode-button appearance="primary">Button Text</vscode-button>
-      </div>
+      </Resizable>
+      {/* TODO additional controls */}
+      <div class="w-[500px]"></div>
     </div>
   );
 }
---
+++ src/web-view-vite/src/lib/Resizable.tsx
@@ -0,0 +1,58 @@
+import { makeEventListener } from "@solid-primitives/event-listener";
+import { ParentProps, createEffect, createSignal } from "solid-js";
+
+export interface ResizableProps extends ParentProps {
+  initialWidth: number;
+}
+
+export function Resizable(props: ResizableProps) {
+  const [offset, setOffset] = createSignal(0);
+
+  createEffect(() => {
+    console.log(offset());
+  });
+
+  function initDragHandle(handle: HTMLDivElement, direction: 1 | -1) {
+    makeEventListener(handle, "mousedown", (startEvent) => {
+      startEvent.preventDefault();
+
+      let lastX = startEvent.clientX;
+      const disposeDragHandle = makeEventListener(
+        window,
+        "mousemove",
+        (currentEvent) => {
+          currentEvent.preventDefault();
+          setOffset((it) => it + (currentEvent.clientX - lastX) * direction);
+          lastX = currentEvent.clientX;
+        }
+      );
+
+      const disposeEndDrag = makeEventListener(window, "mouseup", () => {
+        disposeDragHandle();
+        disposeEndDrag();
+      });
+    });
+  }
+
+  function initResizer(el: HTMLDivElement) {
+    createEffect(() => {
+      el.style.width = `${props.initialWidth + offset() * 2}px`;
+    });
+  }
+
+  return (
+    <div class="relative max-w-[100vw]" ref={initResizer}>
+      {props.children}
+      <div
+        class="absolute w-[10px] cursor-w-resize"
+        style={{ inset: "0px auto 0px -5px" }}
+        ref={(el) => initDragHandle(el, -1)}
+      />
+      <div
+        class="absolute w-[10px] cursor-w-resize"
+        style={{ inset: "0px -5px 0px auto" }}
+        ref={(el) => initDragHandle(el, 1)}
+      />
+    </div>
+  );
+}
