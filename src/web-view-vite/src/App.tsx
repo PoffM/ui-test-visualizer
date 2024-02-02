6b6f51b408b236b2cdc314d3fd00d57240b9656e
PoffM <matp@live.ca>
2024-02-02 13:40:47 -0500

    Implemented color theme in the WebView

--- src/web-view-vite/src/App.tsx
+++ src/web-view-vite/src/App.tsx
@@ -1,7 +1,11 @@
 import { makeEventListener } from "@solid-primitives/event-listener";
 import * as webviewToolkit from "@vscode/webview-ui-toolkit";
+import { Moon, Sun } from "lucide-solid";
 import { createSignal } from "solid-js";
+import { render } from "solid-js/web";
 import { updateDomReplica, type HTMLPatch } from "../../replicate-dom";
+import shadowCSSText from "./assets/shadow.css?raw";
+import { createColorTheme } from "./lib/color-theme";
 
 // In order to use the Webview UI Toolkit web components they
 // must be registered with the browser (i.e. webview) using the
@@ -13,13 +17,39 @@ webviewToolkit
 export function App() {
   const [firstPatchReceived, setFirstPatchReceived] = createSignal(false);
 
-  function initShadow(host: HTMLDivElement) {
+  const controls = document.createElement("div");
+
+  async function initShadow(host: HTMLDivElement) {
     const shadow = host.attachShadow({ mode: "open" });
     shadow.appendChild(
       new DOMParser().parseFromString(
         `<html style="height: 100%; overflow-y: scroll;"><head></head><body></body></html>`,
         "text/html"
       ).children[0]!
+    );
+
+    const shadowStyle = document.createElement("style");
+    shadowStyle.textContent = shadowCSSText;
+    shadow.children[0]!.children[0]!.appendChild(shadowStyle);
+
+    const [theme, toggleTheme] = createColorTheme(
+      shadow.children[0] as HTMLHtmlElement
+    );
+
+    render(
+      () => (
+        <div class="flex ">
+          <vscode-button
+            class="h-10 w-10"
+            appearance="secondary"
+            onClick={toggleTheme}
+            title={`Switch to ${theme() === "dark" ? "light" : "dark"} mode.`}
+          >
+            {theme() === "dark" ? <Moon /> : <Sun />}
+          </vscode-button>
+        </div>
+      ),
+      controls
     );
 
     makeEventListener(window, "message", (event) => {
@@ -32,7 +62,7 @@ export function App() {
   }
 
   return (
-    <div class="fixed inset-0 p-2 pt-[30px]">
+    <div class="fixed inset-0 pt-[30px]">
       <div class="relative h-full w-full">
         <div
           style={{ visibility: firstPatchReceived() ? "hidden" : "visible" }}
@@ -47,7 +77,7 @@ export function App() {
           <div ref={initShadow} class="h-full w-full" />
         </div>
       </div>
-      {/* TODO additional controls */}
+      <div class="fixed bottom-0 p-4 flex flex-col justify-end">{controls}</div>
     </div>
   );
 }
---
+++ src/web-view-vite/src/lib/color-theme.ts
@@ -0,0 +1,68 @@
+import { createEffect, from } from "solid-js";
+
+export type Theme = "dark" | "light" | "system";
+
+/**
+ * Creates a color theme signal.
+ *
+ * Stores the theme in the `<html>` root node's "data-theme" and "class" attributes:
+ * e.g. `<html data-theme="dark" class="dark">`.
+ *
+ * Saves the last selected theme in local storage.
+ */
+export function createColorTheme(
+  root: HTMLHtmlElement
+): [() => Theme, () => void] {
+  const storedTheme =
+    localStorage.getItem("visual-ui-test-debugger.theme") === "dark"
+      ? "dark"
+      : "light";
+
+  const initialTheme =
+    storedTheme ??
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light");
+
+  root.dataset.theme ||= initialTheme;
+
+  const theme = from<Theme>((set) => {
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations.filter(
+        (it) => it.attributeName === "data-theme"
+      )) {
+        const target = mutation.target as HTMLElement;
+        const newTheme: Theme =
+          target.dataset.theme === "dark" ? "dark" : "light";
+        set(newTheme);
+      }
+    });
+    observer.observe(root, {
+      attributes: true,
+      attributeFilter: ["data-theme"],
+    });
+
+    set(root.dataset.theme === "dark" ? "dark" : "light");
+
+    return () => observer.disconnect();
+  });
+
+  createEffect(() => {
+    const _theme = theme();
+    if (_theme) {
+      if (!root.classList.contains(_theme)) {
+        root.classList.remove("dark", "light");
+        root.classList.add(_theme);
+      }
+
+      localStorage.setItem("visual-ui-test-debugger.theme", _theme);
+    }
+  });
+
+  return [
+    () => theme() ?? initialTheme,
+    () =>
+      void (root.dataset.theme =
+        root.dataset.theme === "dark" ? "light" : "dark"),
+  ];
+}
