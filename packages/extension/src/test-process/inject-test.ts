69196c6dc95fb9154dc832c61c3c2a92827b657a
PoffM <matp@live.ca>
2024-02-13 03:52:58 -0500

    improve error logging in jest

--- packages/extension/src/test-process/inject-test.ts
+++ packages/extension/src/test-process/inject-test.ts
@@ -1,5 +1,6 @@
 // Inject this code into the test process
 
+import { error as logError } from 'node:console'
 import { findUpSync } from 'find-up'
 import { createSyncFn } from 'synckit'
 import { initPrimaryDom } from 'replicate-dom'
@@ -12,43 +13,48 @@ const WebSocket
   = require('../../node_modules/ws/index.js') as typeof globalThis.WebSocket
 
 async function preTest() {
-  const client = new WebSocket(
-    `ws://localhost:${process.env.HTML_UPDATER_PORT}`,
-  )
+  try {
+    const client = new WebSocket(
+      `ws://localhost:${process.env.HTML_UPDATER_PORT}`,
+    )
 
-  // Wait for the WebSocket to connect before continuing the test setup.
-  await new Promise<void>((res) => {
-    client.addEventListener('open', () => res())
-  })
-
-  let testWindow: Window = globalThis.window
-
-  function initDom() {
-    initPrimaryDom({
-      root: testWindow.document,
-      onMutation: htmlPatch => client.send(JSON.stringify(htmlPatch)),
-      classes: globalThis.window,
+    // Wait for the WebSocket to connect before continuing the test setup.
+    await new Promise<void>((res) => {
+      client.addEventListener('open', () => res())
     })
 
-    loadStylesIntoHead(testWindow)
+    let testWindow: Window = globalThis.window
+
+    function initDom() {
+      initPrimaryDom({
+        root: testWindow.document,
+        onMutation: htmlPatch => client.send(JSON.stringify(htmlPatch)),
+        classes: globalThis.window,
+      })
+
+      loadStylesIntoHead(testWindow)
+    }
+
+    // Hook into the window which is set by happy-dom or jsdom
+    Object.defineProperty(globalThis, 'window', {
+      get() {
+        return testWindow
+      },
+      set(newWindow: Window) {
+        if (newWindow !== testWindow) {
+          testWindow = newWindow
+          initDom()
+        }
+      },
+      configurable: true,
+    })
+
+    if (testWindow) {
+      initDom()
+    }
   }
-
-  // Hook into the window which is set by happy-dom or jsdom
-  Object.defineProperty(globalThis, 'window', {
-    get() {
-      return testWindow
-    },
-    set(newWindow: Window) {
-      if (newWindow !== testWindow) {
-        testWindow = newWindow
-        initDom()
-      }
-    },
-    configurable: true,
-  })
-
-  if (testWindow) {
-    initDom()
+  catch (error) {
+    logError(error)
   }
 }
 
@@ -126,11 +132,11 @@ if (process.env.TEST_FRAMEWORK === 'jest') {
     const majorNum = Number(major)
 
     if (majorNum < 28) {
-      throw new Error(
-        `Jest version must be 28 or higher, found ${jestPkg.version}.
+      const msg = `Jest version must be 28 or higher, found ${jestPkg.version}.
 When using Jest, this extension relies on support for "setupFiles" to export an async function, introduced in Jest 28.
-https://github.com/jestjs/jest/releases/tag/v28.0.0-alpha.6`,
-      )
+https://github.com/jestjs/jest/releases/tag/v28.0.0-alpha.6`
+      logError(msg)
+      throw new Error(msg)
     }
   })()
 }
