d225bff4a7da672742e124d79273d1899689ca06
PoffM <matp@live.ca>
2024-02-15 04:28:00 -0500

    added more ignored methods to fix some tests

--- packages/replicate-dom/src/primary/mutable-dom-props.ts
+++ packages/replicate-dom/src/primary/mutable-dom-props.ts
@@ -1,4 +1,4 @@
-import type { IWindow } from 'happy-dom'
+import type { Document, IWindow } from 'happy-dom'
 import { uniq } from 'lodash'
 
 export interface DOMNodeSpyConfig<T> {
@@ -19,6 +19,8 @@ export function MUTABLE_DOM_PROPS(
   win: Window & typeof globalThis,
 ): MutableDomDescriptorMap {
   const windowValues = Object.values(win) as unknown[]
+
+  // Get Node and its subclasses, e.g. Element, HTMLElement, HTMLButtonElement, etc.
   const domClasses = uniq(
     windowValues.filter(
       val => val === win.Node
@@ -29,20 +31,35 @@ export function MUTABLE_DOM_PROPS(
 
   const map: MutableDomDescriptorMap = new Map()
 
+  // Loop through Node and its subclasses to find all the mutable properties.
   for (const cls of domClasses) {
-    map.set(cls, { mutableProps: [] })
-    const descriptors = Object.getOwnPropertyDescriptors(
-      Reflect.get(cls, 'prototype') as Node,
-    )
-
-    for (const [prop, desc] of Object.entries(descriptors)) {
-      if (Reflect.has(IGNORED_DESCRIPTORS, prop)) {
-        continue
+    for (
+      let proto: object | null = cls;
+      proto;
+      proto = Reflect.getPrototypeOf(proto)
+    ) {
+      if (map.has(proto) || proto === win.EventTarget) {
+        break
       }
 
-      if (desc.set || typeof desc.value === 'function') {
-        // @ts-expect-error The prop should exist on the object
-        map.get(cls)!.mutableProps.push({ prop, desc })
+      if (!map.has(proto)) {
+        map.set(proto, { mutableProps: [] })
+      }
+
+      // Get the descriptors, i.e. the properties defined in the class definition.
+      const descriptors = Object.getOwnPropertyDescriptors(
+        Reflect.get(proto, 'prototype') as Node,
+      )
+
+      for (const [prop, desc] of Object.entries(descriptors)) {
+        if (Reflect.has(IGNORED_DESCRIPTORS, prop)) {
+          continue
+        }
+
+        if (desc.set || typeof desc.value === 'function') {
+          // @ts-expect-error The prop should exist on the object
+          map.get(proto)!.mutableProps.push({ prop, desc })
+        }
       }
     }
   }
@@ -110,7 +127,12 @@ type ObjectKeys =
   | 'propertyIsEnumerable'
 
 type NodeFunctionKeys =
-  (NodeFunctionKeysMap[keyof NodeFunctionKeysMap] | FunctionKeys<Node> | ObjectKeys)
+  (
+    NodeFunctionKeysMap[keyof NodeFunctionKeysMap]
+    | FunctionKeys<Node>
+    | FunctionKeys<Document>
+    | ObjectKeys
+  )
   & string
 
 /**
@@ -125,6 +147,163 @@ const IGNORED_DESCRIPTORS: { [key in NodeFunctionKeys]?: true } = {
   hasOwnProperty: true,
   isPrototypeOf: true,
   propertyIsEnumerable: true,
+
+  createElement: true,
+  createAttribute: true,
+  createAttributeNS: true,
+  createComment: true,
+  createDocumentFragment: true,
+  createElementNS: true,
+  createEvent: true,
+  createNodeIterator: true,
+  createProcessingInstruction: true,
+  createRange: true,
+  createSVGAngle: true,
+  createSVGLength: true,
+  createSVGNumber: true,
+  createSVGPoint: true,
+  createSVGRect: true,
+  createSVGTransform: true,
+  createTextNode: true,
+  createTreeWalker: true,
+
+  onreadystatechange: true,
+  onpointerlockchange: true,
+  onpointerlockerror: true,
+  onbeforecopy: true,
+  onbeforecut: true,
+  onbeforepaste: true,
+  onfreeze: true,
+  onresume: true,
+  onsearch: true,
+  onvisibilitychange: true,
+  onfullscreenchange: true,
+  onfullscreenerror: true,
+  onwebkitfullscreenchange: true,
+  onwebkitfullscreenerror: true,
+  onbeforexrselect: true,
+  onabort: true,
+  onbeforeinput: true,
+  onblur: true,
+  oncancel: true,
+  oncanplay: true,
+  oncanplaythrough: true,
+  onchange: true,
+  onclick: true,
+  onclose: true,
+  oncontextlost: true,
+  oncontextmenu: true,
+  oncontextrestored: true,
+  oncuechange: true,
+  ondblclick: true,
+  ondrag: true,
+  ondragend: true,
+  ondragenter: true,
+  ondragleave: true,
+  ondragover: true,
+  ondragstart: true,
+  ondrop: true,
+  ondurationchange: true,
+  onemptied: true,
+  onended: true,
+  onerror: true,
+  onfocus: true,
+  onformdata: true,
+  oninput: true,
+  oninvalid: true,
+  onkeydown: true,
+  onkeypress: true,
+  onkeyup: true,
+  onload: true,
+  onloadeddata: true,
+  onloadedmetadata: true,
+  onloadstart: true,
+  onmousedown: true,
+  onmouseenter: true,
+  onmouseleave: true,
+  onmousemove: true,
+  onmouseout: true,
+  onmouseover: true,
+  onmouseup: true,
+  onmousewheel: true,
+  onpause: true,
+  onplay: true,
+  onplaying: true,
+  onprogress: true,
+  onratechange: true,
+  onreset: true,
+  onresize: true,
+  onscroll: true,
+  onsecuritypolicyviolation: true,
+  onseeked: true,
+  onseeking: true,
+  onselect: true,
+  onslotchange: true,
+  onstalled: true,
+  onsubmit: true,
+  onsuspend: true,
+  ontimeupdate: true,
+  ontoggle: true,
+  onvolumechange: true,
+  onwaiting: true,
+  onwebkitanimationend: true,
+  onwebkitanimationiteration: true,
+  onwebkitanimationstart: true,
+  onwebkittransitionend: true,
+  onwheel: true,
+  onauxclick: true,
+  ongotpointercapture: true,
+  onlostpointercapture: true,
+  onpointerdown: true,
+  onpointermove: true,
+  onpointerrawupdate: true,
+  onpointerup: true,
+  onpointercancel: true,
+  onpointerover: true,
+  onpointerout: true,
+  onpointerenter: true,
+  onpointerleave: true,
+  onselectstart: true,
+  onselectionchange: true,
+  onanimationend: true,
+  onanimationiteration: true,
+  onanimationstart: true,
+  ontransitionrun: true,
+  ontransitionstart: true,
+  ontransitionend: true,
+  ontransitioncancel: true,
+  oncopy: true,
+  oncut: true,
+  onpaste: true,
+  onbeforematch: true,
+  onafterprint: true,
+  onbeforeprint: true,
+  onbeforeunload: true,
+  onhashchange: true,
+  onlanguagechange: true,
+  onmessage: true,
+  onmessageerror: true,
+  onoffline: true,
+  ononline: true,
+  onpagehide: true,
+  onpageshow: true,
+  onpopstate: true,
+  onrejectionhandled: true,
+  onstorage: true,
+  onunhandledrejection: true,
+  onunload: true,
+  onanimationcancel: true,
+  oncompositionend: true,
+  oncompositionstart: true,
+  oncompositionupdate: true,
+  onfocusin: true,
+  onfocusout: true,
+  ongamepadconnected: true,
+  ongamepaddisconnected: true,
+  ontouchcancel: true,
+  ontouchend: true,
+  ontouchmove: true,
+  ontouchstart: true,
 
   getAttribute: true,
   getRootNode: true,
@@ -178,11 +357,8 @@ const IGNORED_DESCRIPTORS: { [key in NodeFunctionKeys]?: true } = {
   captureStream: true,
   checkEnclosure: true,
 
-  createSVGNumber: true,
-  createSVGLength: true,
-  createSVGAngle: true,
-  createSVGPoint: true,
-  createSVGRect: true,
-  createSVGTransform: true,
   item: true,
+  assignedElements: true,
+  assignedNodes: true,
+
 }
