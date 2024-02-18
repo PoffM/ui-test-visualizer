fa61b8d9e224563ddb8386e8265c85e722a46389
PoffM <matp@live.ca>
2024-02-17 22:43:02 -0500

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,19 @@ const config = require('@antfu/eslint-config').default
 
 module.exports = config({
   rules: {
+    // My prefs
     'no-console': 'off',
     'no-use-before-define': 'off',
-    'node/prefer-global/process': 'off',
-    'antfu/no-import-node-modules-by-path': 'off',
     'curly': ['error', 'all'],
     'ts/no-use-before-define': 'off',
     'style/jsx-one-expression-per-line': 'off',
     'style/yield-star-spacing': ['error', 'after'],
-
     // Named callback functions are useful for debugging;
     // they are labelled in stack traces.
     'prefer-arrow-callback': 'off',
+
+    // Convenient for this project
+    'node/prefer-global/process': 'off',
+    'antfu/no-import-node-modules-by-path': 'off',
   },
 })
