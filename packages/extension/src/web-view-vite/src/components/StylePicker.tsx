67654124d9ead4fbd442cf9e7a4d9f7a5be2f54c
PoffM <matp@live.ca>
2024-02-22 03:23:50 -0500

    moved popover code into the StylePicker

--- packages/extension/src/web-view-vite/src/components/StylePicker.tsx
+++ packages/extension/src/web-view-vite/src/components/StylePicker.tsx
@@ -1,85 +1,109 @@
-import { For, createResource } from 'solid-js'
+import type { JSX } from 'solid-js'
+import { For, createEffect, createResource, createSignal } from 'solid-js'
 import '../index.css'
 import get from 'lodash/get'
 import { X } from 'lucide-solid'
 import { client } from '../lib/panel-client'
+import { Popover, PopoverContent, PopoverTrigger } from './popover'
 
 export interface StylePickerProps {
-  onOkClick: () => void
+  button: JSX.Element
 }
 
 export function StylePicker(props: StylePickerProps) {
+  const [stylePickerOpen, setStylePickerOpen] = createSignal(false)
+
   const [files, fileQuery] = createResource(
-    () => client.availableCssFiles.query(),
+    stylePickerOpen,
+    isOpen => isOpen ? client.availableCssFiles.query() : undefined,
   )
+
+  const [filesWereChanged, setFilesWereChanged] = createSignal(false)
+
+  createEffect(() => {
+    if (!stylePickerOpen() && filesWereChanged()) {
+      // TODO replace the styles
+    }
+  })
 
   async function toggleFile(path: string, enabled: boolean) {
     const optimisticUpdate = files()
       ?.map(f => f.path === path ? { ...f, enabled } : f)
     fileQuery.mutate(optimisticUpdate)
     await client.toggleCssFile.mutate({ path, enabled })
+    setFilesWereChanged(true)
   }
 
   async function addExternalFiles() {
     await client.addExternalCssFiles.mutate()
+    setFilesWereChanged(true)
     fileQuery.refetch()
   }
 
   async function removeExternalFile(path: string) {
     await client.removeExternalCssFile.mutate(path)
+    setFilesWereChanged(true)
     fileQuery.refetch()
   }
 
   function ok() {
-    props.onOkClick()
+    setStylePickerOpen(false)
   }
 
   return (
-    <div class="flex flex-col gap-2 max-w-[300px]">
-      <h1 class="font-bold text-sm text-center">Enable your styles</h1>
-      <div class="">
-        <For each={files()} fallback={<div>Loading...</div>}>
-          {file => (
-            <div class="flex gap-2">
-              <label class="grow flex justify-between items-center px-2 select-none hover:bg-accent cursor-pointer">
-                <div class="" title={file.path}>
-                  {file.displayPath}
+    <Popover
+      open={stylePickerOpen()}
+      onOpenChange={open => setStylePickerOpen(open)}
+    >
+      <PopoverTrigger>{props.button}</PopoverTrigger>
+      <PopoverContent>
+        <div class="flex flex-col gap-2 max-w-[300px]">
+          <h1 class="font-bold text-sm text-center">Enable your styles</h1>
+          <div class="">
+            <For each={files()} fallback={<div>Loading...</div>}>
+              {file => (
+                <div class="flex gap-2">
+                  <label class="grow flex justify-between items-center px-2 select-none hover:bg-accent cursor-pointer">
+                    <div class="" title={file.path}>
+                      {file.displayPath}
+                    </div>
+                    <vscode-checkbox
+                      checked={file.enabled}
+                      onChange={(e: unknown) => {
+                        const checked = Boolean(get(e, 'currentTarget.checked'))
+                        return toggleFile(file.path, checked)
+                      }}
+                    />
+                  </label>
+                  <vscode-button
+                    appearance="icon"
+                    title="Remove"
+                    onClick={() => removeExternalFile(file.path)}
+                  >
+                    <X />
+                  </vscode-button>
                 </div>
-                <vscode-checkbox
-                  checked={file.enabled}
-                  onChange={(e: unknown) => {
-                    const checked = Boolean(get(e, 'currentTarget.checked'))
-                    return toggleFile(file.path, checked)
-                  }}
-                />
-              </label>
-              <vscode-button
-                appearance="icon"
-                title="Remove"
-                onClick={() => removeExternalFile(file.path)}
-              >
-                <X />
-              </vscode-button>
-            </div>
-          )}
-        </For>
-      </div>
-      <div class="flex justify-between px-2">
-        <vscode-button
-          appearance="secondary"
-          title="Link another file"
-          onClick={addExternalFiles}
-        >
-          Add external file
-        </vscode-button>
-        <vscode-button
-          appearance="primary"
-          title="Link another file"
-          onClick={ok}
-        >
-          OK
-        </vscode-button>
-      </div>
-    </div>
+              )}
+            </For>
+          </div>
+          <div class="flex justify-between px-2">
+            <vscode-button
+              appearance="secondary"
+              title="Link another file"
+              onClick={addExternalFiles}
+            >
+              Add external file
+            </vscode-button>
+            <vscode-button
+              appearance="primary"
+              title="Link another file"
+              onClick={ok}
+            >
+              OK
+            </vscode-button>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
   )
 }
--- packages/extension/src/web-view-vite/src/components/Toolbar.tsx
+++ packages/extension/src/web-view-vite/src/components/Toolbar.tsx
@@ -1,23 +1,20 @@
 import { Brush, Moon, RefreshCw, Sun } from 'lucide-solid'
-import { createSignal } from 'solid-js'
 import { firstPatchReceived, refreshShadow, theme, toggleTheme } from '../App'
-import { Popover, PopoverContent, PopoverTrigger } from './popover'
 import { StylePicker } from './StylePicker'
 
 export function Toolbar() {
-  const [stylePickerOpen, setStylePickerOpen] = createSignal(false)
-
   return (
     <div
       class="flex gap-2 p-2"
       style={{ visibility: firstPatchReceived() ? 'visible' : 'hidden' }}
-    ><vscode-button
-      appearance="secondary"
-      onClick={refreshShadow}
-      title="Refresh Html"
     >
-      <RefreshCw />
-     </vscode-button>
+      <vscode-button
+        appearance="secondary"
+        onClick={refreshShadow}
+        title="Refresh Html"
+      >
+        <RefreshCw />
+      </vscode-button>
       <vscode-button
         class="h-10 w-10"
         appearance="secondary"
@@ -26,11 +23,8 @@ export function Toolbar() {
       >
         {theme() === 'dark' ? <Moon /> : <Sun />}
       </vscode-button>
-      <Popover
-        open={stylePickerOpen()}
-        onOpenChange={open => setStylePickerOpen(open)}
-      >
-        <PopoverTrigger>
+      <StylePicker
+        button={(
           <vscode-button
             class="h-10 w-10"
             appearance="secondary"
@@ -38,11 +32,8 @@ export function Toolbar() {
           >
             <Brush />
           </vscode-button>
-        </PopoverTrigger>
-        <PopoverContent>
-          <StylePicker onOkClick={() => setStylePickerOpen(false)} />
-        </PopoverContent>
-      </Popover>
+        )}
+      />
     </div>
   )
 }
