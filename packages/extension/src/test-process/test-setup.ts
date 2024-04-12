00479f03e9864b54001da638a38595b229e3260d
PoffM <matp@live.ca>
2024-04-12 14:56:45 -0400

    remove unused import

--- packages/extension/src/test-process/test-setup.ts
+++ packages/extension/src/test-process/test-setup.ts
@@ -1,7 +1,6 @@
 // Run this script at the beginning of the test process
 
 import { log, error as logError } from 'node:console'
-import { findUpSync } from 'find-up'
 import { createSyncFn } from 'synckit'
 import { initPrimaryDom, serializeDomNode } from 'replicate-dom'
 import { z } from 'zod'
