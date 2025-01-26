3f4b042ee287f8b405ad4cd7ce128c3c6a320107
PoffM <matp@live.ca>
2025-01-26 00:17:33 -0500

    dep updates

--- packages/web-view-vite/vite.config.ts
+++ packages/web-view-vite/vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), tailwindcss()],
   build: {
     outDir: '../../build-prod/web-view-vite',
     rollupOptions: {
