ae09bf8a57f249db341bcf56aef6ec7800bea6ce
PoffM <matp@live.ca>
2025-08-03 20:43:10 -0400

    try to update rect position on scroll / other events

--- packages/web-view-vite/src/inspector/Inspector.tsx
+++ packages/web-view-vite/src/inspector/Inspector.tsx
@@ -1,4 +1,4 @@
-import { Show, createEffect, createSignal, onCleanup, onMount } from 'solid-js'
+import { Show, createEffect, createMemo, createSignal, onCleanup, onMount } from 'solid-js'
 import { createStore, reconcile } from 'solid-js/store'
 import { makeEventListener } from '@solid-primitives/event-listener'
 import { createMutationObserver } from '@solid-primitives/mutation-observer'
@@ -102,24 +102,36 @@ export function Inspector() {
       </Show>
       <Show when={hoveredNode() ?? selectedNode()} keyed>
         {(node) => {
-          const rect = node instanceof Text
-            ? (() => {
-                const range = document.createRange()
-                range.selectNodeContents(node)
-                return range.getBoundingClientRect()
-              })()
-            : node instanceof Element
-              ? node.getBoundingClientRect()
-              : node.parentElement?.getBoundingClientRect()
+          function newRect() {
+            return node instanceof Text
+              ? (() => {
+                  const range = document.createRange()
+                  range.selectNodeContents(node)
+                  return range.getBoundingClientRect()
+                })()
+              : node instanceof Element
+                ? node.getBoundingClientRect()
+                : node.parentElement?.getBoundingClientRect()
+          }
+
+          const [rect, setRect] = createSignal(newRect())
+
+          // Update the rect on scroll, click, etc
+          makeEventListener(window, 'resize', () => setRect(newRect()))
+          makeEventListener(window, 'scroll', () => setRect(newRect()))
+          makeEventListener(window, 'wheel', () => setRect(newRect()))
+          makeEventListener(window, 'mousedown', () => setRect(newRect()))
+          makeEventListener(window, 'mouseup', () => setRect(newRect()))
+          makeEventListener(window, 'click', () => setRect(newRect()))
 
           return (
             <div
               class="fixed z-50 pointer-events-none bg-[var(--vscode-editorLightBulbAutoFix-foreground)] opacity-60 transition-all duration-100 min-h-[1px] min-w-[1px]"
               style={{
-                top: `${rect?.top}px`,
-                left: `${rect?.left}px`,
-                width: `${rect?.width}px`,
-                height: `${rect?.height}px`,
+                top: `${rect()?.top}px`,
+                left: `${rect()?.left}px`,
+                width: `${rect()?.width}px`,
+                height: `${rect()?.height}px`,
               }}
             />
           )
