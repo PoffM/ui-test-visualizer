3759d43abf19e5eae3ddc63d7dbd1053b6bb5d14
PoffM <matp@live.ca>
2025-08-13 01:39:15 -0400

    do test-block detection with acorn parser instead of using jest-editor-support

--- packages/extension/src/code-lens-provider.ts
+++ packages/extension/src/code-lens-provider.ts
@@ -1,55 +1,57 @@
-import type { ParsedNode } from 'jest-editor-support'
-import { ItBlock, parse } from 'jest-editor-support'
+import { walk } from 'estree-walker'
+// import { parseSync as oxcParse } from 'oxc-parser'
+import { Parser as AcornParser } from 'acorn'
+import { tsPlugin as acornTsPlugin } from '@sveltejs/acorn-typescript'
+import path from 'pathe'
 import type * as vscode from 'vscode'
 import { CodeLens, Range } from 'vscode'
-import get from 'lodash/get'
-import path from 'pathe'
+
+interface TestBlock {
+  name: string
+  span: [number, number]
+}
+
+const AcornTsParser = AcornParser.extend(acornTsPlugin({ jsx: true }))
 
 export const codeLensProvider: vscode.CodeLensProvider = {
   provideCodeLenses(document) {
     try {
-      const rootNode = parse(document.fileName, document.getText(), {
-        plugins: { decorators: 'legacy' },
-      }).root
-
       const codeLenses: vscode.CodeLens[] = []
-      for (const node of allNodes(rootNode)) {
-        if (node instanceof ItBlock) {
-          const range = new Range(
-            node.start.line - 1,
-            node.start.column,
-            node.end.line - 1,
-            node.end.column,
-          )
 
-          // Get the first and last lines in the test block
-          const startAndEndLines = (() => {
-            const bodyLen = get(node, ['testBlock', 'body', 'length'])
-            if (!(typeof bodyLen === 'number')) {
-              return null
+      const code = document.getText()
+
+      // const parsed = oxcParse(document.fileName, code, {})
+      const parsed = AcornTsParser.parse(code, {
+        ecmaVersion: 'latest',
+        // sourceType: 'module',
+        allowAwaitOutsideFunction: true,
+        allowHashBang: true,
+        allowImportExportEverywhere: true,
+        allowReserved: true,
+        allowReturnOutsideFunction: true,
+        allowSuperOutsideMethod: true,
+      })
+
+      const testBlocks: TestBlock[] = []
+      walk(parsed, {
+        enter(node) {
+          if (node.type === 'CallExpression' && ['it', 'fit', 'test'].includes(node?.callee.name)) {
+            const name = node?.arguments?.[0]?.value
+            if (typeof name === 'string') {
+              testBlocks.push({ name, span: [node.start, node.end] })
             }
+          }
+        },
+      })
 
-            // First line in the test
-            const start: unknown = get(node, ['testBlock', 'body', '0', 'loc', 'start', 'line'])
-
-            // Last line in the test
-            const end: unknown = get(node, ['testBlock', 'body', bodyLen - 1, 'loc', 'end', 'line'])
-
-            if (!(typeof start === 'number' && typeof end === 'number')) {
-              return null
-            }
-
-            return [start, end]
-          })()
-
-          codeLenses.push(
-            new CodeLens(range, {
-              arguments: [path.resolve(node.file), node.name, startAndEndLines],
-              title: 'Visually Debug UI',
-              command: 'ui-test-visualizer.visuallyDebugUI',
-            }),
-          )
-        }
+      for (const { name, range } of spansToVsCodeRanges(code, testBlocks)) {
+        codeLenses.push(
+          new CodeLens(range, {
+            arguments: [path.resolve(document.fileName), name, [range.start.line, range.end.line]],
+            title: 'Visually Debug UI',
+            command: 'ui-test-visualizer.visuallyDebugUI',
+          }),
+        )
       }
 
       return codeLenses
@@ -61,10 +63,37 @@ export const codeLensProvider: vscode.CodeLensProvider = {
   },
 }
 
-/** Iterate over all nodes in the tree. */
-function* allNodes(node: ParsedNode): Generator<ParsedNode> {
-  yield node
-  for (const child of node.children ?? []) {
-    yield* allNodes(child)
+function spansToVsCodeRanges(
+  code: string,
+  testBlocks: TestBlock[],
+): { name: string, range: Range }[] {
+  // Find newline indices using regex
+  const newlineIndices: number[] = []
+  for (const match of code.matchAll(/\n/g)) {
+    newlineIndices.push(match.index!)
   }
+
+  function findLineAndColumn(pos: number): [number, number] {
+    let low = 0; let high = newlineIndices.length - 1
+    while (low <= high) {
+      const mid = (low + high) >> 1
+      if (newlineIndices[mid]! < pos) {
+        low = mid + 1
+      }
+      else {
+        high = mid - 1
+      }
+    }
+    const line = low
+    const col = pos - (low > 0 ? newlineIndices[low - 1]! + 1 : 0) + 1 // 1-based col
+    return [line, col]
+  }
+
+  return testBlocks.map(({ name, span: [start, end] }) => ({
+    name,
+    range: new Range(
+      ...findLineAndColumn(start),
+      ...findLineAndColumn(end),
+    ),
+  }))
 }
