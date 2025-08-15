ad05bc7f65e9deda59be958498281d2ff2dada56
PoffM <matp@live.ca>
2025-08-15 16:54:15 -0400

    use vitest 4 beta in the example

--- examples/vitest-react-tailwind4/test/basic.test.tsx
+++ examples/vitest-react-tailwind4/test/basic.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import { Counter } from '../components/Counter'
 
 it('simple react testing library test', () => {
-  setupUi() // For some reason with Vitest 3, you need to press the Step Over button 3 times if you call 'render' directly here.
+  setupUi() // For some reason with Vitest 3 and 4, you need to press the Step Over button 3 times if you call 'render' directly here.
 
   fireEvent.click(screen.getByText('Increment'))
   fireEvent.click(screen.getByText('Increment'))
