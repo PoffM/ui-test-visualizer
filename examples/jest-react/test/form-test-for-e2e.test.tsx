f859d02cfc16bf4dd7540c1119f5136b2d4dd624
PoffM <matp@live.ca>
2025-10-19 00:24:57 -0400

    recorder WIP
    add bun + solid example package

--- examples/jest-react/test/form-test-for-e2e.test.tsx
+++ examples/jest-react/test/form-test-for-e2e.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { FormExample } from '../components/FormExample'
 
-describe('form test for e2e', () => {
+describe('form test for e2e test', () => {
   it('basic usage', async () => {
     render(<FormExample />)
     expect(screen.getByLabelText('First input')).toBeTruthy()
--- examples/vitest-react-tailwind4/test/form-test-for-e2e.test.tsx
+++ examples/vitest-react-tailwind4/test/form-test-for-e2e.test.tsx
@@ -2,7 +2,7 @@ import { describe, it } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { FormExample } from '../components/FormExample'
 
-describe('form test for e2e', () => {
+describe('form test for e2e test', () => {
   it('basic usage', async () => {
     setupUi()
     expect(screen.getByLabelText('First input')).toBeTruthy()
